ab2ae6d862b52a4c8e443efa5f5eef9943cfa6b0
DOTHMANE <delassi.othmane03@gmail.com>
2025-02-12 14:08:18 +0100

    add property validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,7 @@
 class Property < ApplicationRecord
+  # Validations related to property fields
+  validates :latitude, :longitude, :property_type, :marketing_type, presence: true
+  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
+
 end
