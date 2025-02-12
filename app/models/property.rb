19c2c00d06dbbe4a892fa02ed10275dc22f3e2fb
DOTHMANE <delassi.othmane03@gmail.com>
2025-02-12 20:41:57 +0100

    add method to set earth_location value after property creation

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,8 +4,18 @@ class Property < ApplicationRecord
   validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 
+  after_save :set_earth_location
+
   scope :similar_in_radius, ->(property, radius) {
     where(property_type: property.property_type, marketing_type: property.marketing_type)
       .where("earth_distance(ll_to_earth(?, ?), ll_to_earth(latitude, longitude)) <= ?", property.latitude, property.longitude, radius)
   }
+
+
+  private
+  # Method to set the earth_location based on latitude and longitude
+  def set_earth_location
+    result = ActiveRecord::Base.connection.execute("SELECT ll_to_earth(#{latitude}, #{longitude})")
+    update_column(:earth_location, result.first['ll_to_earth'])
+  end
 end
